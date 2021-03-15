b1c20301213b1ac12c836b68b25e67982c30b9c9
Vincent <vincent-lavallee@live.com>
2021-03-15 12:08:05 -0400

    Changes to Checkout

--- Includes/Checkout.php
+++ Includes/Checkout.php
@@ -1,2 +1,16 @@
 <?php
-    session_start();
+    require_once 'Includes/SessionChecker.php'; 
+    require_once 'Includes/dbh.php';
+
+    if(isset($_POST['checkout'])) {
+        $params = array($_SESSION['Id']);
+
+        $sql = "exec Checkout";
+                  
+        $stmt = sqlsrv_query($conn, $sql, $params);
+    }
+    else {
+        header('Location:Panier.php');
+        $_SESSION['checkoutError'] = 'Impossible de finaliser la commande';
+        exit();
+    }
