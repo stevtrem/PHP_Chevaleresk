0cf9b0a6501bc55dda226c96be0c63db52a57e06
Vincent <vincent-lavallee@live.com>
2021-03-22 11:52:39 -0400

    Template

--- Template --Use this.php
+++ Template --Use this.php
@@ -77,8 +77,15 @@
       <section >
         Write code here 
       </section>
+      <!-- Javascript files-->
       <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
       <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
       <script src="js/custom.js"></script>
+      <script src="js/form.js"></script>
    </body>
 </html>
