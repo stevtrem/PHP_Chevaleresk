3a783b236dd6e6fd76ecfd4c3f9590f6df999633
Samuel-Villeneuve <samuel.villeneuve1998@gmail.com>
2021-03-30 11:36:25 -0400

    ajout page inventory

--- shop.php
+++ shop.php
@@ -138,6 +138,9 @@ $accessCheck = isset($_SESSION["UnauthorizedAccess"]) ? $_SESSION["UnauthorizedA
                            <td style="font-weight:bold">
                               {$nomItem}
                            </td>
+                           <td>
+                              <a class="addBtnBoutique" href="Includes/addItemPanier.php?item={$idItem}">Ajouter</a>
+                           </td>
                         </tr>
                      HTML;
                   }  
